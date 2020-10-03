b7bf15e7bdc97d3dd5714e9c2f3fe3f464c0b964
ChristopherFont <64507939+ChristopherFont@users.noreply.github.com>
2020-10-03 16:12:18 -0400

    Update trimVideoRepeatedly.bat

--- trimVideoRepeatedly.bat
+++ trimVideoRepeatedly.bat
@@ -5,9 +5,10 @@ set /p loopAmount="Input Amount of videos + 1: "
 set /A timeAmount = %timeAmount% * 60
 set /A startVidTime=0
 
-
 for /L %%i IN (0, 1, %loopAmount%) DO (
 	set /A startVidTime=%timeAmount%*%%i
+	::If having issues with ffmpeg switch ffmpeg with the location of ffmpeg.exe or whatever
+	::I'm tired and need money.
 	ffmpeg -i %source% -ss !startVidTime! -t %timeAmount% -c:v copy -c:a copy %source%-%%i.mp4
 	)
 ENDLOCAL
