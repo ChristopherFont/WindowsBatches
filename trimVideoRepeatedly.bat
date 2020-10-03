3cd500eb37ce56c0e075e693b2e338e4bfb61f7f
ChristopherFont <64507939+ChristopherFont@users.noreply.github.com>
2020-10-03 16:10:01 -0400

    Add files via upload

---
+++ trimVideoRepeatedly.bat
@@ -0,0 +1,13 @@
+setlocal enabledelayedexpansion
+set /p source="Input source file: "
+set /p timeAmount="Input Amount of Time between cuts (in mins): "
+set /p loopAmount="Input Amount of videos + 1: "
+set /A timeAmount = %timeAmount% * 60
+set /A startVidTime=0
+
+
+for /L %%i IN (0, 1, %loopAmount%) DO (
+	set /A startVidTime=%timeAmount%*%%i
+	ffmpeg -i %source% -ss !startVidTime! -t %timeAmount% -c:v copy -c:a copy %source%-%%i.mp4
+	)
+ENDLOCAL
